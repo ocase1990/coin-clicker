0e98ebe56328773d4ed3b836b09088f48c5b903a
Casey OKeefe <the0surfer@aol.com>
2018-04-06 12:54:35 -0500

    Login form with client-side validation

--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,11 +1,20 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import LoginForm from '../forms/LoginForm';
 
-const LoginPage = () => (
-  <div>
-    <h1> Login Page </h1>
+class LoginPage extends React.Component {
 
-  </div>
-);
+  submit = (data) =>{
+   console.log(data);
+ }
+  render() {
+    return (
+      <div>
+        <h1> Login Page </h1>
+
+        <LoginForm submit={this.submit}/>
+      </div>
+    );
+  }
+};
 
 export default LoginPage;
